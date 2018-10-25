e34ed1c5567d2b5ee8c3756d915500b8019cd5ca
Chicas Poderosas <chicaspoderosas@FCYL-103-31.local>
2018-10-25 11:24:22 -0300

    update

--- script_tallerR_chicaspoderosas.R
+++ script_tallerR_chicaspoderosas.R
@@ -1,5 +1,5 @@
 # Taller de Introducción a R | Chicas Poderosas: nuevos medios, nuevas voces
-# Jueves 25 de octubre de 2018
+# Jueves 25 de octubre de 2018 | 11:30 - 13:30
 # Riva Quiroga | RLadies Santiago | @rivaquiroga | riva@rladies.org
 
 ### SCRIPT TALLER ###
