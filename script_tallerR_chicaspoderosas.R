0f12d5373582824dafcc46957fc55025e65affdc
Riva Quiroga <riva@rladies.org>
2018-10-24 23:30:47 -0300

    update

--- script_tallerR_chicaspoderosas.R
+++ script_tallerR_chicaspoderosas.R
@@ -1,4 +1,5 @@
 # Taller de Introducción a R | Chicas Poderosas: nuevos medios, nuevas voces
+# Jueves 25 de octubre de 2018
 # Riva Quiroga | RLadies Santiago | @rivaquiroga | riva@rladies.org
 
 ### SCRIPT TALLER ###
